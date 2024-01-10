51d5571a05d011222bb9dea8c6e9bc40c22ae208
jasoncmisquith <jsnmisquith2@gmail.com>
2024-01-10 22:22:52 +0530

    Update index.ts

--- socketio/index.ts
+++ socketio/index.ts
@@ -12,17 +12,62 @@ const jc = JSONCodec();
 const nc = await connect({
     servers: ["nats://localhost:4222"]
 })
-const opts = consumerOpts()
-opts.orderedConsumer()
-stream = nc.jetstream();
 
-const sub = await nc.jetstream().subscribe('accountid', opts)
-for await (const msg of sub) {
-    console.log(msg);
-    const data = jc.decode(msg.data);
-    console.log(data)
-    // document.getElementById('1').insertAdjacentHTML('afterend', `<p>${JSON.stringify(data)}</p>`)
-}
+const stream = "CASEUPDATE";
+const consumer = "COMPLETE";
+
+// const opts = consumerOpts()
+// opts.orderedConsumer()
+const js = nc.jetstream();
+const completed_consumer = await js.consumers.get(stream, consumer);
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//       io.emit('chat message', msg);
+//     });
+//   });
+
+while (true) {
+    console.log('executing');
+    console.log("waiting for messages");
+    const messages = await completed_consumer.consume();
+    try {
+      for await (const m of messages) {
+        const msg = jc.decode(m.data);
+        msg['seq'] = m.seq;
+        console.log(msg);
+        console.log(m.seq);
+        console.log(m.subject);
+        io.emit(m.subject, msg);
+        // m.nak();
+        m.ack();
+      }
+    } catch (err: any) {
+      console.log(`consume failed: ${err.message}`);
+    }
+  }
+
+// for await (const msg of sub) {
+//     console.log(msg);
+//     const data = jc.decode(msg.data);
+//     console.log(data);
+//     // document.getElementById('1').insertAdjacentHTML('afterend', `<p>${JSON.stringify(data)}</p>`)
+// }
 
 
 // const app = express();
